cc30a853277f9fe74b1f85b59f0065662dd86c22
prashar <neeraj.prashar@gmail.com>
2016-02-27 13:46:11 -0800

    adding express server

---
+++ NodejsWebApp1/server.js
@@ -0,0 +1,14 @@
+﻿var http = require("http");
+var express = require("express");
+var app = express();
+
+var server = http.createServer(app);
+app.get("/", function (req, res) {
+    res.send("<html><body><h1>" + "Express says Hello" + "</h1></body></html>"); 
+});
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json"); 
+    res.send({ name: "Neeraj", isValid: true, group: "admin" });
+});
+server.listen(3000); 
